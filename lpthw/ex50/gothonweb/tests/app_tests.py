1036a11528c56f956ed12bb5dfed3cca92d3f786
K_B <fengkunbin@126.com>
2018-03-02 19:53:38 +0800

    GameEngine

--- lpthw/ex50/gothonweb/tests/app_tests.py
+++ lpthw/ex50/gothonweb/tests/app_tests.py
@@ -3,7 +3,7 @@ from bin.app import app
 from tests.tools import assert_response
 
 def test_index():
-	# check that we get a 404 on the / URL
+	'''	# check that we get a 404 on the / URL
 	resp = app.request("/")
 	assert_response(resp, status="404")
 
@@ -19,3 +19,6 @@ def test_index():
 	data = {'name': 'Zed', 'greet': 'Hola'}
 	resp = app.request("/hello", method="POST", data=data)
 	assert_response(resp, contains="Zed")
+	'''
+	resp = app.request("/")
+	assert_response(resp, status="303", contains="")
