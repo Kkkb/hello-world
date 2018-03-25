796bf4adea221f7e67c83ca1bc90ece021eb1a64
K_B <fengkunbin@126.com>
2018-03-25 19:08:32 +0800

    a little backup script

---
+++ A-Byte-of-Python/backup_ver5.py
@@ -0,0 +1,57 @@
+import os
+import time
+import sys
+
+argv1 = sys.argv[1]
+argv2 = sys.argv[2]
+argv2 = [argv2]
+
+source = [r'f:\gakki']
+source.extend(argv2)
+target_dir = r'f:\backup'
+
+if not os.path.exists(target_dir):
+	os.mkdir(target_dir)
+
+today = target_dir + os.sep + time.strftime('%Y%m%d')
+
+now = time.strftime('%H%M%S')
+
+comment = input('Enter a comment --> ')
+
+if len(comment) == 0:
+	target = today + os.sep + now + '.zip'
+else:
+	target = today + os.sep + now + '_' + \
+		comment.replace(' ', '_') + '.zip'
+
+if argv1 == '-v':
+	if not os.path.exists(today):
+		os.mkdir(today)
+		print('Successfully created directory', today)
+
+	zip_command = "zip -r {0} {1}".format(target,
+											  ' '.join(source))
+
+	print('Zip command is:')
+	print(zip_command)
+	print('Running:')
+	if os.system(zip_command) == 0:
+		print('Successful backup to', target)
+	else:
+		print('Backup FAILED')
+
+if argv1 == '-q':
+	if not os.path.exists(today):
+		os.mkdir(today)
+#		print('Successfully created directory', today)
+	zip_command = "zip -r {0} {1}".format(target,
+										  ' '.join(source))
+#	print('Zip command is:')
+	zip_command
+#	print('Running:')
+	if os.system(zip_command) == 0:
+		pass
+#	print('Successful backup to', target)
+	else:
+		print('Backup FAILED')
