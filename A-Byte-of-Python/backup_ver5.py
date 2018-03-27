90586f2d60e44ad25be60a5e0fd78bf1aefcb70a
K_B <fengkunbin@126.com>
2018-03-27 17:58:33 +0800

    IO File Pickle Unicode Exception more...

--- A-Byte-of-Python/backup_ver5.py
+++ A-Byte-of-Python/backup_ver5.py
@@ -1,7 +1,27 @@
 import os
 import time
 import sys
+import zipfile
 
+source = [r'f:\gakki']
+target_dir = r'f:\zipbackup'
+
+today = target_dir + os.sep + time.strftime('%Y%m%d')
+
+now = time.strftime('%H%M%S')
+
+comment = input('Enter a comment --> ')
+
+if len(comment) == 0:
+	target = today + os.sep + now + '.zip'
+else:
+	target = today + os.sep + now + '_' + \
+		comment.replace(' ', '_') + '.zip'
+
+with zipfile.ZipFile(target, 'w') as myzip:
+	myzip.write(source)
+
+'''
 argv1 = sys.argv[1]
 argv2 = sys.argv[2]
 argv2 = [argv2]
@@ -55,3 +75,4 @@ if argv1 == '-q':
 #	print('Successful backup to', target)
 	else:
 		print('Backup FAILED')
+'''
