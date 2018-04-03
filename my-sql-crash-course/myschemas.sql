a74e0a27653a9d1e488d63dc677edcd8adc99342
K_B <fengkunbin@126.com>
2018-04-03 15:48:08 +0800

    CREATE VIEW ... AS ...

--- my-sql-crash-course/myschemas.sql
+++ my-sql-crash-course/myschemas.sql
@@ -575,3 +575,42 @@ WHERE customers.cust_id = orders.cust_id
 SELECT cust_name, cust_contact
 FROM productcustomers
 WHERE prod_id = 'TNT2';
+
+SELECT concat(RTrim(vend_name), ' (', RTrim(vend_country), ')') AS vend_title
+FROM vendors
+ORDER BY vend_name;
+
+CREATE VIEW vendorlocations AS
+SELECT Concat(RTrim(vend_name), ' (', RTrim(vend_country), ')') AS vend_title
+FROM vendors
+ORDER BY vend_name;
+
+SELECT *
+FROM vendorlocations;
+
+CREATE VIEW customeremaillist AS
+SELECT cust_id, cust_name, cust_email
+FROM customers
+WHERE cust_email IS NOT NULL;
+
+SELECT *
+FROM customeremaillist;
+
+SELECT prod_id,
+	   quantity,
+       item_price,
+       quantity*item_price AS expanded_price
+FROM orderitems
+WHERE order_num = 20005;
+
+CREATE VIEW orderitemsexpanded AS
+SELECT order_num,
+	   prod_id,
+       quantity,
+       item_price,
+       quantity*item_price AS expanded_price
+FROM orderitems;
+
+SELECT *
+FROM orderitemsexpanded
+WHERE order_num = 20005;
